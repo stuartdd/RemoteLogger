ca43d92c84c0c2b2ae4f3f390a8ca9cc384776be
802996013 <stuart.d.davies@bt.com>
2018-05-21 16:57:37 +0100

    Initial

---
+++ src/main/java/Listen.java
@@ -0,0 +1,8 @@
+public class Listen {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Listening");
+    }
+}
